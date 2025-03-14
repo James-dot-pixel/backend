5bdb9c525fc060379fe2c3773b922ab5d222b49d
James Defosse <jam.defosse@gmail.com>
2025-03-14 09:39:23 +0100

    created controllers to get and post one book

--- controllers/books.js
+++ controllers/books.js
@@ -8,3 +8,29 @@ exports.getBooks = async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 };
+
+exports.postOneBook = async (req, res) => {
+  try {
+    delete req.body._id;
+    const book = new Book({
+      ...req.body,
+    });
+    await book.save();
+    res.status(201).json({ message: "Objet enregistré !" });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+};
+
+exports.getOneBook = async (req, res) => {
+  try {
+    const thing = await Thing.findOne({ _id: req.params.id });
+    if (thing) {
+      res.status(200).json(thing);
+    } else {
+      res.status(404).json({ error: "Book not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+};
--- routes/books.js
+++ routes/books.js
@@ -3,35 +3,13 @@ const router = express.Router();
 
 const booksCtrl = require("../controllers/books");
 
-// Route pour poster un livre
-router.post("/", async (req, res) => {
-  try {
-    delete req.body._id;
-    const book = new Book({
-      ...req.body,
-    });
-    await book.save();
-    res.status(201).json({ message: "Objet enregistré !" });
-  } catch (error) {
-    res.status(400).json({ error });
-  }
-});
-
 // Route pour récupérer tous les livres
 router.get("/", booksCtrl.getBooks);
 
+// Route pour poster un livre
+router.post("/", booksCtrl.postOneBook);
+
 // Route pour récupérer un seul livre
-router.get("/:id", async (req, res) => {
-  try {
-    const thing = await Thing.findOne({ _id: req.params.id });
-    if (thing) {
-      res.status(200).json(thing);
-    } else {
-      res.status(404).json({ error: "Book not found" });
-    }
-  } catch (error) {
-    res.status(400).json({ error });
-  }
-});
+router.get("/:id", booksCtrl.getOneBook);
 
 module.exports = router;
